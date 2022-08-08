73f3a7cf8bb6f405f39dc1c81f3d86d8ba7948a6
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-08-08 14:21:21 -0700

    feat: add fees validation at initialization
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- src/contracts/StakingContract.sol
+++ src/contracts/StakingContract.sol
@@ -144,8 +144,16 @@ contract StakingContract {
     ) external init(1) {
         StakingContractStorageLib.setAdmin(_admin);
         StakingContractStorageLib.setTreasury(_treasury);
+
+        if (_globalFee > BASIS_POINTS) {
+            revert InvalidFee();
+        }
         StakingContractStorageLib.setGlobalFee(_globalFee);
+        if (_operatorFee > BASIS_POINTS) {
+            revert InvalidFee();
+        }
         StakingContractStorageLib.setOperatorFee(_operatorFee);
+
         StakingContractStorageLib.setELDispatcher(_elDispatcher);
         StakingContractStorageLib.setCLDispatcher(_clDispatcher);
         StakingContractStorageLib.setDepositContract(_depositContract);
