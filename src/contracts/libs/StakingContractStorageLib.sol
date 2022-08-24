0f3da06447b46c03fd9103c737d4c72d0d35fcd9
mortimr <iulian@rotaru.fr>
2022-08-24 09:21:51 +0200

    fix: add unused bytes32 unstructure storage utils

--- src/contracts/libs/StakingContractStorageLib.sol
+++ src/contracts/libs/StakingContractStorageLib.sol
@@ -26,18 +26,6 @@ library StakingContractStorageLib {
         }
     }
 
-    function getBytes32(bytes32 position) internal view returns (bytes32 data) {
-        assembly {
-            data := sload(position)
-        }
-    }
-
-    function setBytes32(bytes32 position, bytes32 data) internal {
-        assembly {
-            sstore(position, data)
-        }
-    }
-
     /* ========================================
     ===========================================
     =========================================*/
