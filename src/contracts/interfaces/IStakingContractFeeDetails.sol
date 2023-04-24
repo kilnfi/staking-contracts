eb61616b285874ec8ee63c1d576ebc055cd04497
0xvv <public_double_v@protonmail.com>
2023-04-24 13:13:30 +0200

    fix spearbit-audits/review-kiln#34

--- src/contracts/interfaces/IStakingContractFeeDetails.sol
+++ src/contracts/interfaces/IStakingContractFeeDetails.sol
@@ -4,8 +4,6 @@ pragma solidity >=0.8.10;
 interface IStakingContractFeeDetails {
     function getWithdrawerFromPublicKeyRoot(bytes32 _publicKeyRoot) external view returns (address);
 
-    function getLastWithdrawFromPublicKeyRoot(bytes32 _publicKeyRoot) external view returns (uint256);
-
     function getTreasury() external view returns (address);
 
     function getOperatorFeeRecipient(bytes32 pubKeyRoot) external view returns (address);
