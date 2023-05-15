a74d0810a2c97b2eaaa7763bd347ed30eed2b7e2
0xvv <public_double_v@protonmail.com>
2023-05-15 12:00:45 +0200

    deployment: ledger komainu mainnet deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -49,6 +49,12 @@ const hhuc: HardhatUserConfig = {
       accounts: {
         mnemonic: process.env.MNEMONIC || "",
       },
+    },
+    mainnet_komainu: {
+      url: process.env.RPC_URL || "",
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
     }
   },
   dodoc: {
@@ -70,7 +76,8 @@ const hhuc: HardhatUserConfig = {
       goerli_live: "0x3B9B2C07eff60aC828117C997E04c61890Ad2Ed7",
       mainnet_vault: "0x3c69D70ea8487a7E64127a6Eae194ada4C144318",
       mainnet_live: "0xd235d4Eb3A483743C506C8AB6ee50f4eBfDEF4D8",
-      mainnet_enzyme: "0xb270FE91e8E4b80452fBF1b4704208792A350f53"
+      mainnet_enzyme: "0xb270FE91e8E4b80452fBF1b4704208792A350f53",
+      mainnet_komainu: "0xd235d4Eb3A483743C506C8AB6ee50f4eBfDEF4D8"
     },
     admin: {
       default: 2,
@@ -78,7 +85,8 @@ const hhuc: HardhatUserConfig = {
       goerli_live: "0xC4b8469165d0A0e0939500BdeCE7c0CD3415a9fb",
       mainnet_vault: "0xD3269B4daBd8AA336155F741C534CBAC87526A8E",
       mainnet_live: "0xCf53Ef5be9C713585D2fEF40e72D9c7C4fE1D5F2",
-      mainnet_enzyme: "0x45DAD754897ef0b2780349AD7c7000c72717b24E"
+      mainnet_enzyme: "0x45DAD754897ef0b2780349AD7c7000c72717b24E",
+      mainnet_komainu: "0xCf53Ef5be9C713585D2fEF40e72D9c7C4fE1D5F2"
     },
     depositContract: {
       default: 4,
@@ -87,6 +95,7 @@ const hhuc: HardhatUserConfig = {
       mainnet_vault: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
       mainnet_live: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
       mainnet_enzyme: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
+      mainnet_komainu: "0x00000000219ab540356cBB839Cbe05303d7705Fa"
     },
     treasury: {
       default: 5,
@@ -94,7 +103,8 @@ const hhuc: HardhatUserConfig = {
       goerli_live: "0x5137B5540730d44326fBb237184425A9FB311DdF",
       mainnet_vault: "0x2C8C8e8022827a97388C6Ae9C22FF26EA2f02542",
       mainnet_live: "0xd3947210779c046D5ADCE2a6665d650450A56280",
-      mainnet_enzyme: "0x1ad1fc9964c551f456238Dd88D6a38344B5319D7"
+      mainnet_enzyme: "0x1ad1fc9964c551f456238Dd88D6a38344B5319D7",
+      mainnet_komainu: "0xCdB0570d55Ebe8c8d678e090F86fa73729EF8Fc7"
     },
   },
 };
