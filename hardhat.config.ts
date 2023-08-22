663f66c7ef01e2d6b027f6fbf96f4664575e8e2b
0xvv <public_double_v@protonmail.com>
2023-08-22 19:41:17 +0200

    fixed deployment scripts & deployed new goerli instance

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,6 +26,12 @@ const hhuc: HardhatUserConfig = {
         mnemonic: process.env.MNEMONIC || "",
       },
     },
+    goerli_consensys_dev: {
+      url: process.env.RPC_URL || "",
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
+    },
     goerli_vault: {
       url: process.env.RPC_URL || "",
       accounts: {
@@ -91,6 +97,7 @@ const hhuc: HardhatUserConfig = {
     proxyAdmin: {
       default: 1,
       goerli_consensys: "0x938e1682fBcd30149f547eca7688ed00724AC3bF",
+      goerli_consensys_dev: "0x938e1682fBcd30149f547eca7688ed00724AC3bF",
       goerli_vault: "0x3B9B2C07eff60aC828117C997E04c61890Ad2Ed7",
       goerli_live: "0x3B9B2C07eff60aC828117C997E04c61890Ad2Ed7",
       mainnet_vault: "0x3c69D70ea8487a7E64127a6Eae194ada4C144318",
@@ -103,6 +110,7 @@ const hhuc: HardhatUserConfig = {
     admin: {
       default: 2,
       goerli_consensys: "0x4Cb0De8A79C766C478742666d024A16E3e81aAE0",
+      goerli_consensys_dev: "0x4Cb0De8A79C766C478742666d024A16E3e81aAE0",
       goerli_vault: "0xC4b8469165d0A0e0939500BdeCE7c0CD3415a9fb",
       goerli_live: "0xC4b8469165d0A0e0939500BdeCE7c0CD3415a9fb",
       mainnet_vault: "0xD3269B4daBd8AA336155F741C534CBAC87526A8E",
@@ -115,6 +123,7 @@ const hhuc: HardhatUserConfig = {
     depositContract: {
       default: 4,
       goerli_consensys: "0xff50ed3d0ec03ac01d4c79aad74928bff48a7b2b",
+      goerli_consensys_dev: "0xff50ed3d0ec03ac01d4c79aad74928bff48a7b2b",
       goerli_vault: "0xff50ed3d0ec03ac01d4c79aad74928bff48a7b2b",
       goerli_live: "0xff50ed3d0ec03ac01d4c79aad74928bff48a7b2b",
       mainnet_vault: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
@@ -127,6 +136,7 @@ const hhuc: HardhatUserConfig = {
     treasury: {
       default: 5,
       goerli_consensys: "0xAb07A64D407c25f02f1a2dc0aF97076630a03F17",
+      goerli_consensys_dev: "0xAb07A64D407c25f02f1a2dc0aF97076630a03F17",
       goerli_vault: "0x73cC0AFEaAc1E6f2C08A7D4484bB5628062558CB",
       goerli_live: "0x5137B5540730d44326fBb237184425A9FB311DdF",
       mainnet_vault: "0x2C8C8e8022827a97388C6Ae9C22FF26EA2f02542",
