6df02b9c7d003504f1b57b7ef6d639ce963943dc
pwnh4 (@loicttn) <loic@ttn.am>
2023-10-26 10:42:11 +0200

    feat(deployments): holesky devnet and testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -61,7 +61,19 @@ const hhuc: HardhatUserConfig = {
       accounts: {
         mnemonic: process.env.MNEMONIC || "",
       },
-    }
+    },
+    holesky_devnet: {
+      url: process.env.RPC_URL || "",
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
+    },
+    holesky_testnet: {
+      url: process.env.RPC_URL || "",
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
+    },
   },
   dodoc: {
     include: [
@@ -84,7 +96,9 @@ const hhuc: HardhatUserConfig = {
       mainnet_vault: "0x3c69D70ea8487a7E64127a6Eae194ada4C144318",
       mainnet_live: "0xd235d4Eb3A483743C506C8AB6ee50f4eBfDEF4D8",
       mainnet_enzyme: "0xb270FE91e8E4b80452fBF1b4704208792A350f53",
-      mainnet_komainu: "0xd235d4Eb3A483743C506C8AB6ee50f4eBfDEF4D8"
+      mainnet_komainu: "0xd235d4Eb3A483743C506C8AB6ee50f4eBfDEF4D8",
+      holesky_devnet: "0xbA8E90E82ae33EBf6ab1c451Fe135546E86D0Eab",
+      holesky_testnet: "0xb597001A2bEC560cBD73a1F02eBfDb86b42aC71B"
     },
     admin: {
       default: 2,
@@ -94,7 +108,9 @@ const hhuc: HardhatUserConfig = {
       mainnet_vault: "0xD3269B4daBd8AA336155F741C534CBAC87526A8E",
       mainnet_live: "0xCf53Ef5be9C713585D2fEF40e72D9c7C4fE1D5F2",
       mainnet_enzyme: "0x45DAD754897ef0b2780349AD7c7000c72717b24E",
-      mainnet_komainu: "0xCf53Ef5be9C713585D2fEF40e72D9c7C4fE1D5F2"
+      mainnet_komainu: "0xCf53Ef5be9C713585D2fEF40e72D9c7C4fE1D5F2",
+      holesky_devnet: "0xb3eb29AC481FCFAFA7008A4acf04737c7d6733EA",
+      holesky_testnet: "0xe6fe1936Fa8120e57c7Dee1733693B59b392672c"
     },
     depositContract: {
       default: 4,
@@ -105,6 +121,8 @@ const hhuc: HardhatUserConfig = {
       mainnet_live: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
       mainnet_enzyme: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
       mainnet_komainu: "0x00000000219ab540356cBB839Cbe05303d7705Fa"
+      holesky_devnet: "0x4242424242424242424242424242424242424242",
+      holesky_testnet: "0x4242424242424242424242424242424242424242"
     },
     treasury: {
       default: 5,
@@ -115,6 +133,8 @@ const hhuc: HardhatUserConfig = {
       mainnet_live: "0xd3947210779c046D5ADCE2a6665d650450A56280",
       mainnet_enzyme: "0x1ad1fc9964c551f456238Dd88D6a38344B5319D7",
       mainnet_komainu: "0xCdB0570d55Ebe8c8d678e090F86fa73729EF8Fc7"
+      holesky_devnet: "0xb3eb29AC481FCFAFA7008A4acf04737c7d6733EA",
+      holesky_testnet: "0xe6fe1936Fa8120e57c7Dee1733693B59b392672c"
     },
   },
 };
